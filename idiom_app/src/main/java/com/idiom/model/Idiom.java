0b0ca5b6189b945fbd1a9db208d02572f68bf38f
richard5zx <richardchao2009@gmail.com>
2024-11-13 22:18:22 +0800

    model part done

--- idiom_app/src/main/java/com/idiom/model/Idiom.java
+++ idiom_app/src/main/java/com/idiom/model/Idiom.java
@@ -1,5 +1,35 @@
 package com.idiom.model;
+import java.sql.Timestamp;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="idiom")
 public class Idiom {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer idiom_id;
+	private String idiom;
+	private String definition;
+	private String language;
+	private Timestamp date_time;
+	
+	public Idiom(String idiom, String definition, String language, Timestamp date_time) {
+		super();
+		this.idiom = idiom;
+		this.definition = definition;
+		this.language = language;
+		this.date_time = date_time;
+	}
+	
+	public Idiom() {
+		super();
+	}
 
 }
--- idiom_app/src/main/java/com/idiom/model/IdiomExample.java
+++ idiom_app/src/main/java/com/idiom/model/IdiomExample.java
@@ -1,5 +1,36 @@
 package com.idiom.model;
 
+import java.sql.Timestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="idiom_example")
 public class IdiomExample {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer idiom_id;
+	private String idiom;
+	private String definition;
+	private String language;
+	private Timestamp date_time;
+	
+	public IdiomExample(String idiom, String definition, String language, Timestamp date_time) {
+		super();
+		this.idiom = idiom;
+		this.definition = definition;
+		this.language = language;
+		this.date_time = date_time;
+	}
+	
+	public IdiomExample() {
+		super();
+	}
 
 }
