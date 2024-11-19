970000070c6dc3905d30cfcee42e071f37fb6b59
richard5zx <richardchao2009@gmail.com>
2024-11-19 18:34:14 +0800

    basic crud implemented in controller

--- idiom_app/src/main/java/com/idiom/controller/IdiomController.java
+++ idiom_app/src/main/java/com/idiom/controller/IdiomController.java
@@ -1,18 +1,53 @@
 package com.idiom.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.idiom.model.Idiom;
 import com.idiom.service.impl.IdiomServiceImpl;
 
 @RestController
-@RequestMapping("Idiom")
+@RequestMapping("idiom")
 @CrossOrigin
 public class IdiomController {
 	@Autowired
 	IdiomServiceImpl idiomServiceImpl;
 	
+	// C
+	@PostMapping("createIdiom")
+	public String createIdiom(@RequestBody Idiom idiom) {
+		idiomServiceImpl.addIdiom(null);
+		return "Idiom Added";
+	}
 	
+	// R
+	@GetMapping("getAllIdiom")
+	public List<Idiom> getAllIdiom() {
+		return idiomServiceImpl.findAllIdiom();
+	}
+	
+	// U
+	@PutMapping("updateIdiom")
+	public String updateIdiom(@RequestBody Idiom idiom) {
+		idiomServiceImpl.updateIdiom(idiom);
+		return "Idiom Updated";
+	}
+	
+	// D
+	@DeleteMapping("deleteIdiom/{idiomId}")
+	public String deleteIdiom(@PathVariable("idiomId") int idiomId) {
+		idiomServiceImpl.deleteIdiom(idiomId);
+		return "Idiom Deleted";
+		
+	}
 }
--- idiom_app/src/main/java/com/idiom/controller/IdiomExampleController.java
+++ idiom_app/src/main/java/com/idiom/controller/IdiomExampleController.java
@@ -1,18 +1,51 @@
 package com.idiom.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.idiom.model.IdiomExample;
 import com.idiom.service.impl.IdiomExampleServiceImpl;
 
 @RestController
-@RequestMapping("IdiomExample")
+@RequestMapping("idiomExample")
 @CrossOrigin
 public class IdiomExampleController {
 	@Autowired
 	IdiomExampleServiceImpl idiomExampleServiceImpl;
 	
+	// C
+	@PostMapping("createIdiomExample")
+	public String createIdiomExample(@RequestBody IdiomExample idiomExample) {
+		idiomExampleServiceImpl.addIdiomExample(idiomExample);
+		return "Example Added";
+	}
 	
+	// R
+	@GetMapping("getAllIdiomExample")
+	public List<IdiomExample> getALlIdiomExample() {
+		return idiomExampleServiceImpl.findAllIdiomExample();
+	}
+	
+	// U
+	@PutMapping("updateIdiomExample")
+	public String updateIdiom(@RequestBody IdiomExample IdiomExample) {
+		idiomExampleServiceImpl.updateIdiomExample(IdiomExample);
+		return "Example Update";
+	}
+	
+	// D
+	@DeleteMapping("deleteIdiomExample/{exampleId}")
+	public String deleteIdiomExample(@PathVariable("exampleId")int exampleID) {
+		return "Example Deleted";
+	}
 }
